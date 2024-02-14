c91da354b59b5accc6bb9c969d8476016d6a305a
Suguru Takahashi <samonikura100@gmail.com>
2024-02-14 18:21:15 +0900

    Initial Commit

---
+++ DISample/DISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DISampleApp.swift
+//  DISample
+//
+//  Created by Suguru Takahashi on 2024/02/14.
+//
+
+import SwiftUI
+
+@main
+struct DISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

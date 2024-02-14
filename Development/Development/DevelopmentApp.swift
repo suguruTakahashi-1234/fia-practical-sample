12021e4be2c8f1cd7191dcba1801139a157be62b
Suguru Takahashi <samonikura100@gmail.com>
2024-02-14 18:58:43 +0900

    first commit

---
+++ Development/Development/DevelopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopmentApp.swift
+//  Development
+//
+//  Created by Suguru Takahashi on 2024/02/14.
+//
+
+import SwiftUI
+
+@main
+struct DevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UICatalog/UICatalog/UICatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  UICatalogApp.swift
+//  UICatalog
+//
+//  Created by Suguru Takahashi on 2024/02/14.
+//
+
+import SwiftUI
+
+@main
+struct UICatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

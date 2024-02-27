1fe3358013b661cdc412f6e999e657b179aba762
Suguru Takahashi <samonikura100@gmail.com>
2024-02-27 18:03:10 +0900

    update script

--- Sourcery/Script/presentation_code_gen.sh
+++ Sourcery/Script/presentation_code_gen.sh
@@ -10,11 +10,11 @@ SOURCERY_PACKAGE_PATH="./DISampleAppPackage"
 SCREEN_NAME="$1"
 TEMPLATE_PATH="./Sourcery/Template"
 SOURCE_DIR="${SOURCERY_PACKAGE_PATH}/Sources/Presentation"
-OUTPUT_BASE_DIR="${SOURCE_DIR}/${SCREEN_NAME}"
+OUTPUT_PRESENTATION_DIR="${SOURCE_DIR}/${SCREEN_NAME}"
 OUTPUT_TEST_DIR="${SOURCERY_PACKAGE_PATH}/Tests/PreviewSnapshotTest"
 
 # 出力ディレクトリが存在しない場合は作成
-mkdir -p "$OUTPUT_BASE_DIR"
+mkdir -p "$OUTPUT_PRESENTATION_DIR"
 
 # 各コンポーネントを生成
 for COMPONENT in "PresenterDependency" "Presenter" "Wireframe" "View"
@@ -22,7 +22,7 @@ do
     swift run --package-path "$SOURCERY_PACKAGE_PATH" mint run sourcery \
               --sources "$SOURCE_DIR" \
               --templates "$TEMPLATE_PATH/${COMPONENT}.stencil" \
-              --output "$OUTPUT_BASE_DIR/${SCREEN_NAME}${COMPONENT}.swift" \
+              --output "$OUTPUT_PRESENTATION_DIR/${SCREEN_NAME}${COMPONENT}.swift" \
               --args "screenName=$SCREEN_NAME"
 done
 

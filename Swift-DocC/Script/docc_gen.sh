b42c5aefee47375e2a9b79e7b477668c8df106f5
Suguru Takahashi <samonikura100@gmail.com>
2024-03-01 04:24:07 +0900

    update

--- Swift-DocC/Script/docc_gen.sh
+++ Swift-DocC/Script/docc_gen.sh
@@ -10,16 +10,16 @@ TARGET=$1
 cd ./DISampleAppPackage
 
 # Ref: https://apple.github.io/swift-docc-plugin/documentation/swiftdoccplugin/publishing-to-github-pages/#Generating-the-Documentation-Site
-swift package --allow-writing-to-directory ../docs \
+swift package --allow-writing-to-directory ./docs \
   generate-documentation --target "$TARGET" \
   --disable-indexing \
   --transform-for-static-hosting \
-  --hosting-base-path "https://github.com/suguruTakahashi-1234/DISample/docs" \
-  --output-path ../docs
+  --hosting-base-path "https://github.com/suguruTakahashi-1234/DISample" \
+  --output-path ./docs
 
-echo "Documentation for $TARGET has been generated and saved to ../docs"
+echo "Documentation for $TARGET has been generated and saved to ./docs"
 
 TARGET_LOWER=$(echo "$1" | tr '[:upper:]' '[:lower:]')
 URL="https://sugurutakahashi-1234.github.io/DISample/documentation/$TARGET_LOWER"
-echo $URL
-open $URL
+echo "After pushing your changes to GitHub, please visit the following URL to view the documentation:"
+echo "$URL"

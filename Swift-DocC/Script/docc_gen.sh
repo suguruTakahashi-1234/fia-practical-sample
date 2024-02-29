7ea54ca54c605ad075c8f757d06512725efa90e0
Suguru Takahashi <samonikura100@gmail.com>
2024-03-01 04:31:56 +0900

    update

--- Swift-DocC/Script/docc_gen.sh
+++ Swift-DocC/Script/docc_gen.sh
@@ -7,15 +7,17 @@ fi
 
 TARGET=$1
 
+cd ./DISampleAppPackage
+
 # Ref: https://apple.github.io/swift-docc-plugin/documentation/swiftdoccplugin/publishing-to-github-pages/#Generating-the-Documentation-Site
-swift package --allow-writing-to-directory ./docs \
+swift package --allow-writing-to-directory ../docs \
   generate-documentation --target "$TARGET" \
   --disable-indexing \
   --transform-for-static-hosting \
-  --hosting-base-path "https://github.com/suguruTakahashi-1234/DISample" \
-  --output-path ./docs
+  --hosting-base-path DISample \
+  --output-path ../docs
 
-echo "Documentation for $TARGET has been generated and saved to ./docs"
+echo "Documentation for $TARGET has been generated and saved to ../docs"
 
 TARGET_LOWER=$(echo "$1" | tr '[:upper:]' '[:lower:]')
 URL="https://sugurutakahashi-1234.github.io/DISample/documentation/$TARGET_LOWER"

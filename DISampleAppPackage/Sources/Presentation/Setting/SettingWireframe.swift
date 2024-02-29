e88157cf5ba1bf140ea40c1b5273d259dd3fd890
Suguru Takahashi <samonikura100@gmail.com>
2024-03-01 02:09:35 +0900

    update stencil

--- DISampleAppPackage/Sources/Presentation/Setting/SettingWireframe.swift
+++ DISampleAppPackage/Sources/Presentation/Setting/SettingWireframe.swift
@@ -2,16 +2,4 @@
 import DomainLayer
 import Foundation
 
-public protocol SettingWireframe {
-    // add transition source Wireframe
-//    associatedtype SettingWireframeAT: SettingWireframe
-//    associatedtype SettingPresenterDependencyAT: SettingPresenterDependency
-//
-//    func createSettingView() -> SettingView<SettingWireframeAT, SettingPresenterDependencyAT>
-
-    // add Router
-//    @MainActor
-//    public func createSettingView() -> SettingView<Self, Dependency> {
-//        SettingView(router: self, dependency: dependency)
-//    }
-}
+public protocol SettingWireframe {}

0cc46304fc561f0c608200c223b5cb68fa1782e0
Suguru Takahashi <samonikura100@gmail.com>
2024-03-01 02:00:31 +0900

    update コメント

--- DISampleAppPackage/Sources/Presentation/Setting/SettingWireframe.swift
+++ DISampleAppPackage/Sources/Presentation/Setting/SettingWireframe.swift
@@ -2,4 +2,16 @@
 import DomainLayer
 import Foundation
 
-public protocol SettingWireframe {}
+public protocol SettingWireframe {
+    // add transition source Wireframe
+//    associatedtype SettingWireframeAT: SettingWireframe
+//    associatedtype SettingPresenterDependencyAT: SettingPresenterDependency
+//
+//    func createSettingView() -> SettingView<SettingWireframeAT, SettingPresenterDependencyAT>
+
+    // add Router
+//    @MainActor
+//    public func createSettingView() -> SettingView<Self, Dependency> {
+//        SettingView(router: self, dependency: dependency)
+//    }
+}

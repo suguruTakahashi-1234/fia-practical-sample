84adab275fcba23755efd63aea502d5cfad1023d
Suguru Takahashi <60458956+suguruTakahashi-1234@users.noreply.github.com>
2024-03-16 00:39:46 +0900

    【オンボーディング画面】オンボーディング画面の実装 #48

---
+++ DISampleAppPackage/Sources/Presentation/View/Onboarding/OnboardingView.swift
@@ -0,0 +1,46 @@
+
+import DomainLayer
+import SwiftUI
+
+@MainActor
+public struct OnboardingView<Router: AppRootWireframe, Dependency: OnboardingPresenterDependency>: View {
+    private let router: Router
+    @StateObject private var presenter: OnboardingPresenter<Dependency>
+
+    public init(router: Router, dependency: Dependency) {
+        self.router = router
+        _presenter = .init(wrappedValue: OnboardingPresenter(dependency: dependency))
+    }
+
+    public var body: some View {
+        NavigationStack {
+            Text("OnboardingView")
+                .navigationTitle("Onboarding")
+                .task {
+                    await presenter.onAppear()
+                }
+                .onDisappear {
+                    presenter.onDisappear()
+                }
+        }
+    }
+}
+
+// MARK: - Preview
+
+import PreviewSnapshots
+
+struct OnboardingView_Previews: PreviewProvider, SnapshotTestable {
+    static var previews: some View {
+        snapshots.previews.previewLayout(.sizeThatFits)
+    }
+
+    static var snapshots: PreviewSnapshots<AppRootRouterDependencyMock> {
+        .init(
+            configurations: configurationAllSizesWithEmpty,
+            configure: { state in
+                OnboardingView(router: AppRootRouter.empty, dependency: state)
+            }
+        )
+    }
+}

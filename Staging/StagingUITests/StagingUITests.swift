277e786c3376b62723c5801ab2837cb3a7d09a77
Suguru Takahashi <60458956+suguruTakahashi-1234@users.noreply.github.com>
2024-03-28 01:49:23 +0900

    【Rename】DependencyInjector -> DependencyInjection #70

--- Staging/StagingUITests/StagingUITests.swift
+++ Staging/StagingUITests/StagingUITests.swift
@@ -13,19 +13,19 @@ final class StagingUITests: XCTestCase {
     func testTaskListView() throws {
         let app = XCUIApplication()
         app.launch()
-        app.tabBars["Tab Bar"].buttons["タスク"].tap()
+        app.tabBars["タブバー"].buttons["タスク"].tap()
     }
 
     func testSettingView() throws {
         let app = XCUIApplication()
         app.launch()
-        app.tabBars["Tab Bar"].buttons["設定"].tap()
+        app.tabBars["タブバー"].buttons["設定"].tap()
     }
 
     func testLicenseListView() throws {
         let app = XCUIApplication()
         app.launch()
-        app.tabBars["Tab Bar"].buttons["設定"].tap()
+        app.tabBars["タブバー"].buttons["設定"].tap()
         app.collectionViews/*@START_MENU_TOKEN@*/ .staticTexts["ライセンス"]/*[[".cells",".buttons[\"ライセンス\"].staticTexts[\"ライセンス\"]",".staticTexts[\"ライセンス\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/ .tap()
         app.navigationBars["ライセンス"].buttons["設定"].tap()
     }
@@ -33,7 +33,7 @@ final class StagingUITests: XCTestCase {
     func testLicenseDetailView() throws {
         let app = XCUIApplication()
         app.launch()
-        app.tabBars["Tab Bar"].buttons["設定"].tap()
+        app.tabBars["タブバー"].buttons["設定"].tap()
         let collectionViewsQuery = app.collectionViews
         collectionViewsQuery/*@START_MENU_TOKEN@*/ .buttons["ライセンス"]/*[[".cells.buttons[\"ライセンス\"]",".buttons[\"ライセンス\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ .tap()
         collectionViewsQuery/*@START_MENU_TOKEN@*/ .buttons["abseil"]/*[[".cells.buttons[\"abseil\"]",".buttons[\"abseil\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/ .tap()
@@ -42,7 +42,7 @@ final class StagingUITests: XCTestCase {
     func testDeviveInfoView() throws {
         let app = XCUIApplication()
         app.launch()
-        app.tabBars["Tab Bar"].buttons["設定"].tap()
+        app.tabBars["タブバー"].buttons["設定"].tap()
 
         let collectionViewsQuery = app.collectionViews
         collectionViewsQuery/*@START_MENU_TOKEN@*/ .staticTexts["デバイス情報"]/*[[".cells",".buttons[\"デバイス情報\"].staticTexts[\"デバイス情報\"]",".staticTexts[\"デバイス情報\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/ .tap()

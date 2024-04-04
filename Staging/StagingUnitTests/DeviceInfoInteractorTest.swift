e188ee2da1987fc61875e40db5a9bfc3274fb83c
Suguru Takahashi <60458956+suguruTakahashi-1234@users.noreply.github.com>
2024-04-04 11:21:54 +0900

    #16 【README】READMEの更新

---
+++ Staging/StagingUnitTests/DeviceInfoInteractorTest.swift
@@ -0,0 +1,25 @@
+//
+//  Created by sugurutakahashi on 2024/04/04
+//  Copyright sugurutakahashi. All rights reserved.
+//
+
+import DependencyInjectionLayer
+import DomainLayer
+@testable import PresentationLayer
+import XCTest
+
+final class DeviceInfoInteractorTest: XCTestCase {
+    var interactor: DeviceInfoInteractor<AppRootDependencyInjector>!
+    var dependencyInjector: AppRootDependencyInjector!
+
+    @MainActor
+    override func setUpWithError() throws {
+        dependencyInjector = .init(buildScheme: .staging)
+        interactor = .init(dependency: dependencyInjector)
+    }
+
+    @MainActor
+    func testGetDeviceInfoValue() {
+        XCTAssertEqual(interactor.getDeviceInfoValue(.buildScheme), "Staging", "デバイス情報が取得できる（Build Scheme）")
+    }
+}

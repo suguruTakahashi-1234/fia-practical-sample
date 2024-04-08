19a6ae90729850f51262bd4fa683c20d604f4167
Suguru Takahashi <60458956+suguruTakahashi-1234@users.noreply.github.com>
2024-04-08 18:30:15 +0900

    【Swift-DocC】docsファイルの .gitignore 設定 #115

--- DISampleAppPackage/Sources/Domain/Framework/Driver/BuildEnvDriver.swift
+++ DISampleAppPackage/Sources/Domain/Framework/Driver/BuildEnvDriver.swift
@@ -7,6 +7,8 @@ import Foundation
 
 public final class BuildEnvDriver: BuildEnvDriverProtocol {
     public let buildScheme: BuildScheme
+
+    /// .debug なのか .release なのかを気にしなくて済むように BuildConfiguration はイニシャライザの引数で指定せず、実際のビルド環境を適応させていて、もし、検証が必要なときは Mock を使う方針（要検討）
     public let buildConfiguration: BuildConfiguration
 
     public init(buildScheme: BuildScheme) {

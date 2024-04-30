920d453b8a58aa0a20ff2763358680f7cfee9a13
Suguru Takahashi <60458956+suguruTakahashi-1234@users.noreply.github.com>
2024-05-01 00:15:35 +0900

    【ミニアプリ】【テスト】UIテスト用のミニアプリを作成する #153

--- DevelopmentViewCatalog/DevelopmentViewCatalog/DevelopmentViewCatalogApp.swift
+++ DevelopmentViewCatalog/DevelopmentViewCatalog/DevelopmentViewCatalogApp.swift
@@ -12,15 +12,7 @@ struct DevelopmentViewCatalogApp: App {
     private let dependency: AppRootDIContainerDependencyMock
 
     init() {
-        // コマンドライン引数で初回起動UIテストモードが有効であれば UserDefaults をクリアにする
-        LocalDataStoreDriver.allClearForFirstLaunchUITest()
-
-        // UIテスト時に固定値であるとテストしやすいのでプレースホルダーモードにするときはそれ用のMockを差し込む（通常時はランダムな値を生成するMockを差し込む）
-        if ProcessInfo.processInfo.arguments.contains(Constants.Arguments.placeholderUITest) {
-            dependency = AppRootDIContainerDependencyMock.placeholder
-        } else {
-            dependency = AppRootDIContainerDependencyMock.random
-        }
+        dependency = AppRootDIContainerDependencyMock.random
     }
 
     var body: some Scene {
